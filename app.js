454775721cd8e52d973e97c21d701a22fb2461d2
LuisMiLR <luismidream@hotmail.com>
2023-06-15 01:18:06 +0200

    navbar fix

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const express = require('express');
+const express = require("express");
 // afficher messages à nos users
-const handlebarsHelpers = require('handlebars-helpers');
-const flash = require ('connect-flash');
-const session = require ('express-session');
-const path = require('path');
+const handlebarsHelpers = require("handlebars-helpers");
+const flash = require("connect-flash");
+const session = require("express-session");
+const path = require("path");
 const mysql = require("mysql");
-const dotenv = require('dotenv');
-const hbs = require('hbs');
-const handlebars = require('handlebars');
-const fs = require('fs');
-const cookieParser = require('cookie-parser');
+const dotenv = require("dotenv");
+const hbs = require("hbs");
+const handlebars = require("handlebars");
+const fs = require("fs");
+const cookieParser = require("cookie-parser");
 
-
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 const app = express();
 
@@ -21,49 +20,44 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
-const publicDirectory = path.join(__dirname, './public');
-const viewsDirectory = path.join(__dirname, './views');
+const publicDirectory = path.join(__dirname, "./public");
+const viewsDirectory = path.join(__dirname, "./views");
 
 app.use(express.static(publicDirectory));
-// 
+//
 
+app.set("view engine", "hbs");
+app.set("views", viewsDirectory);
 
+const header = fs.readFileSync("./views/header.hbs", "utf8");
+const footer = fs.readFileSync("./views/footer.hbs", "utf8");
 
-app.set('view engine', 'hbs');
-app.set('views', viewsDirectory);
-
-
-const header = fs.readFileSync('./views/header.hbs', 'utf8');
-const footer = fs.readFileSync('./views/footer.hbs', 'utf8');
-
-hbs.registerPartial('header', header);
-hbs.registerPartial('footer', footer);
+hbs.registerPartial("header", header);
+hbs.registerPartial("footer", footer);
 
 handlebarsHelpers({ handlebars: hbs.handlebars });
-hbs.registerHelper('ifCond', function(v1,v2,options){
-  if(v1 == v2){
+hbs.registerHelper("ifCond", function (v1, v2, options) {
+  if (v1 == v2) {
     return options.fn(this);
-  }else{
-    return options.inverse(this)
+  } else {
+    return options.inverse(this);
   }
 });
 
 // Definir routes
 
-app.use(session({
-  secret: 'helloguys',
-  saveUninitialized: true,
-  resave: true
-}));
+app.use(
+  session({
+    secret: "helloguys",
+    saveUninitialized: true,
+    resave: true,
+  })
+);
 
 // routes get :
-app.use('/', require('./routes/pages'))
+app.use("/", require("./routes/pages"));
 // routes post :
-app.use('/auth', require('./routes/auth'));
-
-
-
-
+app.use("/auth", require("./routes/auth"));
 
 // app.get('/', (req, res) => {
 //     req.flash('success', `You've been successfully redirected to the Message route!`)
@@ -73,7 +67,6 @@ app.use('/auth', require('./routes/auth'));
 // app.get('/message', (req, res) => {
 //     res.send(req.flash('success'))
 // })
-
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
